7550d17345800af3c5e0e773af8602ac911efdd3
LeHoang510 <lehoangnguyen510@gmail.com>
2022-07-02 06:30:58 +0200

    Update Gemfile for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def hello
+    render html: "hello, world!"
+  end
+
+  def hola
+    render html: "<h1>hola, mundo!<h1>"
+  end
+
+  def goodbye
+    render html: "goodbye"
+  end
 end
